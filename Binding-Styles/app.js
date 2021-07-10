2f180e9424f6459cc6224aa06e351eeb378dd69c
progg <ptrogg1992@gmail.com>
2021-07-09 19:15:28 -0500

    Basic computed property binding classes

--- Binding-Styles/app.js
+++ Binding-Styles/app.js
@@ -3,5 +3,10 @@ let vm = Vue.createApp({
         return {
             isPurple: false
         }
+    },
+    computed: {
+        circle_classes() {
+            return {purple: this.isPurple }
+        }
     }
 }).mount('#app')
